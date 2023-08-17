c2c66e793abb4a0fdc6c40efc91fe8a3b6b73486
Claudio Castro <claudio.87_@hotmail.com>
2023-08-17 16:34:27 -0300

    finales de js

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameOver = false;
 let foodX, foodY;
 let snakeX = 15, snakeY = 15;
 let velocityX = 0, velocityY = 0;
 let snakeBody = [];
-
+let setIntervalId
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over!");
+    location.reload();
+}
+
+
 const changeDirection = (e) => {
    // console.log(e);
-    if(e.key === "ArrowUp") {
+    if(e.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    }else if(e.key === "ArrowDown") {
+    }else if(e.key === "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    }else if(e.key === "ArrowLeft") {
+    }else if(e.key === "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    }else if (e.key === "ArrowRight") {
+    }else if (e.key === "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -30,6 +38,7 @@ const changeDirection = (e) => {
 }
 
 const initGame = () => {
+    if(gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}" ></div>`;
     if(snakeX === foodX && snakeY === foodY) {
         changeFoodPosition();
@@ -45,13 +54,23 @@ const initGame = () => {
     snakeX += velocityX;
     snakeY += velocityY;
 
+    if(snakeX <= 0 || snakeX >30 || snakeY <= 0 || snakeY >30) {
+       gameOver = true;
+    }
+
+
+
+
     for(let i = 0;i < snakeBody.length;i++) {
         htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}" ></div>`;
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && i !== 0 && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
     }
     playBoard.innerHTML = htmlMarkup;
 }
 
 
 changeFoodPosition();
-setInterval(initGame,125);
+setIntervalId = setInterval(initGame,125);
 document.addEventListener("keydown", changeDirection);
